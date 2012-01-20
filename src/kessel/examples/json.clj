fc5b5478ad7a6d4ec348616fd486f75e71fc750d
apgwoz <git@apgwoz.com>
2012-01-19 20:05:11 -0500

    * non working json example, but close

--- src/kessel/examples/json.clj
+++ src/kessel/examples/json.clj
@@ -1,3 +1,22 @@
 (ns kessel.examples.json
-  (:use [kessel :as c]))
+  ^{:doc "This example uses kessel to create a simplified JSON parser"}
+  (:use [kessel]))
 
+(def jkey (lexeme (either natural string-literal)))
+(def jvalue []
+  (doparser [p (lexeme (either jdict jlist natural string-literal))]
+            p))
+
+(defn jkey-value
+  []
+  (doparser [k jkey
+             _ (lexeme colon)
+             v jvalue]
+            [k v]))
+
+(defn jdict
+  []
+  (doparser [kvs (between \{ \} (sep-by jkey-value (lexeme comma)))]
+            (reduce conj {} kvs)))
+
+(def jlist (between \( \) (sep-by jvalue comma)))
