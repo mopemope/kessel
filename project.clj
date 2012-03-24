f7a596fbc84d86893bedb58537adc8d3355bb5e2
mopemoepe <yutaka.matsubara@gmail.com>
2012-03-24 10:44:03 +0900

    Add plugin

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Parsec ported to clojure based on clarsec"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
+  :plugins [[org.clojars.mopemope/lein-cont "0.1.0"]])
