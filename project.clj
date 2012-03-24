cc36d9ecc8948c17ebc62e719ce554780e4ebb70
mopemoepe <yutaka.matsubara@gmail.com>
2012-03-24 10:58:38 +0900

    Fix project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/algo.monads "0.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
-  :plugins [[org.clojars.mopemope/lein-cont "0.1.0"]])
+  :plugins [[org.clojars.mopemope/lein-cont "0.1.1"]]
+  :aot :all
+  :min-lein-version "2.0.0")
